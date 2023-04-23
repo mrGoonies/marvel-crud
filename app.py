13e24dfe3db559512c7f53799dda68045f1a4ab5
mrGoonies <gooniesdev@gmail.com>
2023-04-23 16:46:14 -0400

    Modified: Implement search for a character by its id

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from apistar import App, Route, types, validators
 from apistar.http import JSONResponse
 
 
-def clean_dataset():
+def clean_dataset() -> None:
     data = pd.read_csv('marvel.csv')
     data = data.sort_values('page_id')
     data['page_id'] = data['page_id'].astype(int)
@@ -14,14 +14,13 @@ def clean_dataset():
                           if isinstance(x, str) else x)
     data.to_csv('./datos_limpio.csv', index=False)
 
-def convert_csv_to_json():
+def convert_csv_to_json() -> None:
     with open('datos_limpio.csv', 'r') as csvfile:
         read_csv = csv.DictReader(csvfile)
         list_dict = [row for row in read_csv]
 
         with open('dataset.json', 'w') as jsonfile:
             json.dump(list_dict, jsonfile, indent=4)
-
 
 
 def load_data() -> dict:
@@ -57,6 +56,8 @@ class MarvelCharacter(types.Type):
     year = validators.String(max_length=8)
 
 
+
+
 # API Methods
 def list_character() -> list:
     return [data for data in dataset.items()]
@@ -64,8 +65,18 @@ def list_character() -> list:
 def create_character(marvel_character: MarvelCharacter) -> JSONResponse:
     pass
 
-def get_character(character_id: int) -> JSONResponse:
-    pass
+def get_character(character_id: str) -> JSONResponse:
+    character = dataset.get(character_id)
+
+    if not character:
+        error = {'Error': CHARACTER_NOT_FOUND}
+
+        return JSONResponse(error, 404)
+    
+    return JSONResponse(character, 200)
+
+
+    
 
 def update_character(character_id: int, character: MarvelCharacter) -> JSONResponse:
     pass
